901605f7ed003b12be06d7427fcfa222911b167b
nmuthuswami <nirmalanand.muthuswami@gmail.com>
2020-03-15 16:02:09 +0530

    Modified hello world

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const server = express();
 
 server.get('/',(req,res)=>{
-    res.send('Hello World, Good Evening');
+    res.send('Hello World, Good Morning');
 })
 
 server.listen(3000,()=>{
